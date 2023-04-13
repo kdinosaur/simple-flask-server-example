b52f12383f458aa5e98648af2ace2cfedbf703e3
kdinosaur <9345819+kdinosaur@users.noreply.github.com>
2023-04-13 15:51:50 +0000

    added about page

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def index():
 def hello():
   return render_template('hello.html', name=request.args.get('name'))
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
 @app.errorhandler(404)
 def handle_404(e):
     return '<h1>404</h1><p>File not found!</p><img src="https://httpcats.com/404.jpg" alt="cat in box">', 404
